b38066611671095e4c2a330a4191cd11fd94d8d6
raccoonyy <raccoonyy@gmail.com>
2013-05-31 23:06:39 +0900

    test passed about double parentheses and parallels parentheses

--- calculator.py
+++ calculator.py
@@ -1,49 +1,44 @@
 # -*- coding:utf-8 -*-
-'''
-다음 두 블로그를 참조
-http://erezsh.wordpress.com/2012/11/18/how-to-write-a-calculator-in-50-python-lines-without-eval/
-http://blog.83rpm.com/archives/882
-'''
 import re
 
 
 def MyCalc(exp):
-    def calculator(syms):
-        if len(syms) == 3:
-            return calc(syms)
+    def calculator(symbols, LPAR=0, RPAR=0):
+        if '(' in symbols:
+            LPAR = len(symbols) - symbols[::-1].index('(') - 1
+            RPAR = symbols[LPAR:].index(')') + LPAR
 
-        if '(' in syms:
-            left = syms[::-1].index('(')
-            right = syms.index(')')
-            result = calculator(syms[-left:right])
-            syms.insert(-left-1, str(result))
-            del syms[-left-1:right+2]
-            return calculator(syms)
+            sub_symbols = symbols[LPAR+1:RPAR]
+            del symbols[LPAR:RPAR+1]
+            symbols.insert(LPAR, str(calculator(sub_symbols)))
+            return calculator(symbols)
+
+        if len(symbols) == 3:
+            return operations(symbols)
 
         index = 1  # 순차 계산일 경우
 
         # 우선순위에 따라
-        if '*' in syms:
-            index = syms.index('*')
+        if '*' in symbols:
+            index = symbols.index('*')
 
-        if '/' in syms:
-            index = syms.index('/')
+        if '/' in symbols:
+            index = symbols.index('/')
 
-        result = calc(syms[index-1:index+2])
-        del syms[index-1:index+2]
-        syms.insert(index-1, str(result))
-        return calculator(syms)
+        result = operations(symbols[index-1:index+2])
+        del symbols[index-1:index+2]
+        symbols.insert(index-1, result)
+        return calculator(symbols)
 
-    def calc(syms):
-        if '*' in syms:
-            return int(syms[0]) * int(syms[2])
-        if '/' in syms:
-            return int(syms[0]) / int(syms[2])
-        if '+' in syms:
-            return int(syms[0]) + int(syms[2])
-        if '-' in syms:
-            return int(syms[0]) - int(syms[2])
-
+    def operations(symbols):
+        if '*' in symbols:
+            return int(symbols[0]) * int(symbols[2])
+        if '/' in symbols:
+            return int(symbols[0]) / int(symbols[2])
+        if '+' in symbols:
+            return int(symbols[0]) + int(symbols[2])
+        if '-' in symbols:
+            return int(symbols[0]) - int(symbols[2])
 
     symbols = re.findall('[[\d.]+|\(|\)|\+|\-|\/|\*|]', exp)
     return calculator(symbols)
